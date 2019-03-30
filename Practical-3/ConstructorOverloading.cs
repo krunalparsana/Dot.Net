98f1cd1d2b2473b19f85453d80f4e62e9f268f58
krunalparsana <46141926+krunalparsana@users.noreply.github.com>
2019-03-30 10:02:13 +0530

    Add files via upload

---
+++ Practical-3/ConstructorOverloading.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2
+{
+    public class Student
+    {
+        string name, enrollment, branch;
+        public Student(string name) {
+            this.name = name;
+        }
+        public Student(string name, string enrollment) {
+            this.name = name;
+            this.enrollment = enrollment;
+        }
+        public Student(string name, string enrollment, string branch) {
+            this.name = name;
+            this.enrollment = enrollment;
+            this.branch = branch;
+        }
+        public static void Main(String[] ar) {
+            Student s1 = new Student("Krunal");
+            Student s2 = new Student("Krunal","160470107044");
+            Student s3 = new Student("Krunal","160470107044","Computer");
+        }
+    }
+}
